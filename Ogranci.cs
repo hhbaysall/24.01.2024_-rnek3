c3de1f4ea4ddc630516f72876477994619535149
hhbaysall <hhbaysall@gmail.com>
2024-01-24 22:09:08 +0300

    Add project files.

---
+++ Ogranci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._01._2024_örnek3
+{
+     public   class Ogranci
+    {
+       
+
+        public string Ad { get; set; }
+        public string SoyAd { get; set; }
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+
+        public double Ortalama
+        {
+            get { return (N1 + N2 + N3) / 3.0; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Kullanıcıya Kaç adet öğrenci bilgisi girmek isteğini soralım.
+// O sayı kadar öğrenci bilgisi talep edelim.
+// öğrencilerin genel not ort sini ekrana yazdıralım.
+// ad soyad n1 n2 n3  
+
+using _24._01._2024_örnek3;
+
+Console.Write("Girilecek öğrenci bilgisi girmek istersiniz?  ");
+int adet=int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+List<Ogranci> ogrenciler =new List<Ogranci>();
+
+for (int i = 0; i < adet; i++)
+{
+    Console.Write("{0}. Öğrenci Bilgileri",i+1);
+    Console.WriteLine("----------------------");
+    Ogranci o = new Ogranci();
+    Console.Write("Ad: ");
+    o.Ad = Console.ReadLine();
+    Console.Write("Soy Ad: ");
+    o.SoyAd = Console.ReadLine();
+    Console.Write("1. Notu: ");
+    o.N1=int.Parse(Console.ReadLine());
+    Console.Write("2. Notu: ");
+    o.N2 = int.Parse(Console.ReadLine());
+    Console.Write("3. Notu: ");
+    o.N3 = int.Parse(Console.ReadLine());
+    ogrenciler.Add(o);
+}
+
+double genelOrt = 0;
+
+foreach (var item in ogrenciler)
+{
+    genelOrt += item.Ortalama;
+}
+genelOrt = genelOrt / ogrenciler.Count;
+
+Console.WriteLine("Genel ortalama: "+genelOrt);
